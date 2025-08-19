86ad59ac30eaa8356345d1acc7b632f5a0fd0db7
Juan Baltazar Tapia <juan.space14@gmail.com>
2025-08-19 09:16:00 -0400

    Endcode Decode

---
+++ EncodeDecode.js
@@ -0,0 +1,43 @@
+// Design an algorithm to encode a list of strings to a single string.
+// The encoded string is then decoded back to the original list of strings.
+
+// Please implement encode and decode
+class Solution {
+  /**
+   * @param {string[]} strs
+   * @returns {string}
+   */
+  encode(strs) {
+    const result = [];
+    //insert words length before every word
+    for (const word of strs) {
+      const length = word.length.toString();
+      result.push(length + word);
+    }
+    return result.join("");
+  }
+
+  /**
+   * @param {string} str
+   * @returns {string[]}
+   */
+  decode(str) {
+    //seperate every word based on number before it
+    const result = [];
+    let i = 0;
+    while (i < str.length) {
+      const length = Number(str.charAt(i));
+      result.push(str.slice(i + 1, length + i + 1));
+      i += length + 1;
+    }
+
+    return result;
+  }
+}
+
+const solution = new Solution();
+
+const encoded = solution.encode(["we", "say", ":", "yes"]);
+console.log(encoded);
+const decoded = solution.decode(encoded);
+console.log(decoded);
