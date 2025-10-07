9be58a4664363aa27d544ac04182de9b8a710c7f
Juan Baltazar Tapia <juan.space14@gmail.com>
2025-10-07 14:40:15 -0400

    initital commit

---
+++ MedianOfTwoSortedArrays.js
@@ -0,0 +1,19 @@
+// Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+
+// The overall run time complexity should be O(log (m+n)).
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+//[1,2,3,4,5,6]
+// l
+//
+//[3,4,5,6,7,8]
+//           r
+
+var findMedianSortedArrays = function(nums1, nums2) {
+    
+};
+console.log(findMedianSortedArrays([1,3],[2])) //2.00
+console.log(findMedianSortedArrays([1,2],[3,4])) //2.50
